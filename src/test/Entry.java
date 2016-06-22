5a03cbfdea0d49fb83ac08fcd5df3a9fb0e9e246
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-06-22 18:14:31 -0300

    Adiciona pacote de teste com WebView e JSON

---
+++ src/test/Entry.java
@@ -0,0 +1,63 @@
+package test;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import netscape.javascript.JSObject;
+
+public class Entry extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Web View");
+
+		WebView webview = new WebView();
+		WebEngine engine = webview.getEngine();
+
+		String uri = getClass().getResource("/html/index.html").toExternalForm();
+		engine.load(uri);
+
+		JSObject window = (JSObject) engine.executeScript("window");
+		window.setMember("java", new Bridge());
+
+		Scene scene = new Scene(webview);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public class Bridge {
+		public String loadJSON() {
+			InputStream in = getClass().getResourceAsStream("/html/test.json");
+
+			BufferedInputStream bin = new BufferedInputStream(in);
+			ByteArrayOutputStream buf = new ByteArrayOutputStream();
+			String json = null;
+
+			try {
+				int res = bin.read();
+				while (res != -1) {
+					byte b = (byte) res;
+					buf.write(b);
+					res = bin.read();
+				}
+
+				json = buf.toString("UTF-8");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return json;
+		}
+	}
+}
